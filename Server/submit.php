dacdf399d45737d8f89abf227d7637855bf98597
Sean Lossef <seanlossef@gmail.com>
2020-02-07 20:55:45 -0500

    Update submit.php

--- Server/submit.php
+++ Server/submit.php
@@ -4,6 +4,8 @@ $servername = "localhost";
 $username = "fpm";
 $password = "";
 
+$p = $_GET["p"];
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
 
@@ -12,7 +14,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO data.sharplocker (password) VALUES ('". mysqli_real_escape_string($_GET['p']) ."')";
+$sql = "INSERT INTO data.sharplocker (password) VALUES ('". mysqli_real_escape_string($p) ."')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
